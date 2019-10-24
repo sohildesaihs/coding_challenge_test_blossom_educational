fee00c448ae63a9d8fb8300b6ccc8a56d76d7408
Sohil Desai <sohil.desai.hs@gmail.com>
2019-10-24 19:50:18 +0530

    Initial test case passed for file upload feature.

--- src/Application.php
+++ src/Application.php
@@ -1,27 +1,38 @@
 <?php
+
 namespace Blossom\BackendDeveloperTest;
 
+use mysql_xdevapi\Exception;
+use phpDocumentor\Reflection\File;
+use S3Stub\FileObject;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FTPStub\FTPUploader;
+use S3Stub\Client;
+use DropboxStub\DropboxClient;
+use EncodingStub\Client as EncodeClient;
+use FFMPEGStub\FFMPEG;
 
 /**
  * You should implement this class however you want.
- * 
+ *
  * The only requirement is existence of public function `handleRequest()`
  * as this is what is tested. The constructor's signature must not be changed.
  */
 class Application
 {
+    private $_configParams;
+
     /**
      * By default the constructor takes a single argument which is a config array.
      *
      * You can handle it however you want.
-     * 
+     *
      * @param array $config Application config.
      */
     public function __construct(array $config)
     {
-
+        $this->_configParams = $config;
     }
 
     /**
@@ -29,13 +40,178 @@ class Application
      *
      * You should implement it however you want and it should return a Response
      * that passes all tests found in EncoderTest.
-     * 
-     * @param  Request $request The request.
+     *
+     * @param Request $request The request.
      *
      * @return Response
      */
     public function handleRequest(Request $request): Response
     {
+        $response = new Response();
+        $response->setCharset('UTF-8');
+        $response->headers->set('Content-Type', 'application/json');
 
+        // Request type must be POST
+        if (!$request->isMethod('post')) {
+            $response->setStatusCode(Response::HTTP_METHOD_NOT_ALLOWED)->setContent(json_encode(["message" => "Unsupported request type."]));
+            return $response;
+        }
+
+        // Upload key must exist
+        if (!$request->get("upload")) {
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST)->setContent(json_encode(["message" => "Please select file upload destination."]));
+            return $response;
+        }
+
+        // File must be selected to be uploaded.
+        $file = $request->files->get("file");
+        if (empty($file)) {
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST)->setContent(json_encode(["message" => "Please select a file to upload."]));
+            return $response;
+        }
+
+        // upload file
+        try {
+            $url = $this->uploadOnServer($file, $request->get("upload"));
+
+            // File encoding validations.
+            $formats = null;
+            if (!empty($reqFormats = $request->get("formats"))) {
+                $formats = $this->fileEncode($file, $reqFormats, $request->get("upload"));
+            }
+        } catch (\Exception $ex) {
+            // file upload exception messages.
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST)->setContent(json_encode(["message" => $ex->getMessage()]));
+            return $response;
+        }
+
+        $content = array("url" => $url, "formats" => $formats);
+        $response->setContent(json_encode($content))->setStatusCode(Response::HTTP_OK);
+        return $response;
+    }
+
+    /**
+     * Performs file encoding for supported types.
+     *
+     * @param $file
+     * @param $reqFormats
+     * @param $uploadBucket
+     * @return array
+     * @throws \Exception
+     */
+    private function fileEncode($file, $reqFormats, $uploadBucket)
+    {
+        $return = [];
+        foreach ($reqFormats as $reqFormat) {
+            switch ($reqFormat) {
+                case "mp4":
+                    $mp4Convertor = new FFMPEG();
+                    $file = $mp4Convertor->convert($file);
+                    // upload again converted file.
+                    $return[$reqFormat] = $this->uploadOnServer($file, $uploadBucket);
+                    break;
+                case "gif":
+                    throw new \Exception("Unsupported file format provided for conversion.");
+                    break;
+                default:
+                    $encodingParams = $this->_configParams["encoding.com"];
+                    $encode = new EncodeClient($encodingParams["app_id"], $encodingParams["access_token"]);
+                    $return[$reqFormat] = $encode->encodeFile($file, $reqFormat);
+                    break;
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * Validate and filter upload files to relevant storage.
+     *
+     * @param $file FileObject
+     * @param $type string
+     * @return string
+     * @throws \Exception
+     */
+    private function uploadOnServer($file, $bucketType)
+    {
+        $uploadFileUrl = null;
+        switch ($bucketType) {
+            case UploadBuckets::FTP:
+                $uploadFileUrl = $this->uploadOnFtp($file);
+                break;
+            case UploadBuckets::S3:
+                $uploadFileUrl = $this->uploadOnS3($file);
+                break;
+            case UploadBuckets::DROPBOX:
+                $uploadFileUrl = $this->uploadOnDropbox($file);
+                break;
+            default:
+                throw new \Exception("Unknown upload destination selected.");
+                break;
+        }
+
+        return $uploadFileUrl;
+    }
+
+    /**
+     * Upload file on FTP
+     *
+     * @param $file FileObject
+     * @return string
+     * @throws \Exception
+     */
+    private function uploadOnFtp($file)
+    {
+        $ftpParams = $this->_configParams["ftp"];
+        $host = $ftpParams["hostname"];
+        $user = $ftpParams["username"];
+        $password = $ftpParams["password"];
+        $folder = $ftpParams["destination"];
+
+        $ftp = new FTPUploader($file, $host, $user, $password, $folder);
+
+        // todo: handle exceptions
+
+        if (!$ftp) {
+            throw new \Exception("FTP upload error occurred.");
+        }
+
+        return "ftp://$host/$folder/" . $file->getFileName();
+    }
+
+    /**
+     * Upload file on S3 bucket
+     *
+     * @param $file FileObject
+     * @return string
+     * @throws \Exception
+     */
+    private function uploadOnS3($file)
+    {
+        $s3Params = $this->_configParams["s3"];
+        $s3 = new Client($s3Params["access_key_id"], $s3Params["secret_access_key"]);
+
+        // todo: handle exceptions
+
+        $uploadedFile = $s3->send($file, $s3Params["bucketname"]);
+
+        return $uploadedFile->getPublicUrl();
+    }
+
+    /**
+     * Upload file on Dropbox
+     *
+     * @param $file FileObject
+     * @return string
+     * @throws \Exception
+     */
+    private function uploadOnDropbox($file)
+    {
+        $dropboxParams = $this->_configParams["dropbox"];
+        $dropbox = new DropboxClient($dropboxParams["access_key"], $dropboxParams["secret_token"], $dropboxParams["container"]);
+
+        // todo: handle exceptions
+
+        return $dropbox->upload($file);
     }
 }
---
+++ src/UploadBuckets.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Blossom\BackendDeveloperTest;
+
+/**
+ * Class UploadBuckets
+ * @package Blossom\BackendDeveloperTest
+ */
+class UploadBuckets
+{
+    const FTP = "ftp";
+    const S3 = "s3";
+    const DROPBOX = "dropbox";
+}
